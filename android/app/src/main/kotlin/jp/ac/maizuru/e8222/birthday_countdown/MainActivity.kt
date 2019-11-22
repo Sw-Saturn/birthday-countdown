29261acdeabc9fb3cf593bb183158ff3d45123a9
Kanta Demizu <demizu.1203@gmail.com>
2019-11-22 09:46:47 +0900

    :tada: Initial Commit

---
+++ android/app/src/main/kotlin/jp/ac/maizuru/e8222/birthday_countdown/MainActivity.kt
@@ -0,0 +1,13 @@
+package jp.ac.maizuru.e8222.birthday_countdown
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
